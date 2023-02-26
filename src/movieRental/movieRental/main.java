c95f1cce3673d31b179cf93f93814f3604091ade
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2023-02-26 18:50:17 +0000

    Testing

--- src/movieRental/movieRental/main.java
+++ src/movieRental/movieRental/main.java
@@ -3,9 +3,11 @@ package movieRental;
 public class main {
     public static void main(String[] args) {
 
-        Movie movie = new Movie("Tangled", 1);
+        Movie movie = new Movie("Tangled", 2);
+        Movie movie1 = new Movie("Star Wars", 1);
         Customer customer = new Customer("Ben");
-        customer.addRental(new Rental(movie, 7));
+        customer.addRental(new Rental(movie, 2));
+        customer.addRental(new Rental(movie1, 2));
         System.out.println(customer.htmlStatement());
     }
 }   
